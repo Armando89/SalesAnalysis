568c2b4d59f9a84dd0a81dc9666757c897ae2711
Armando Galan-Cedeño <43739205+Armando89@users.noreply.github.com>
2020-03-24 22:24:54 -0500

    Complete without descriptions

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,13 +33,17 @@
             this.ExitButton = new System.Windows.Forms.Button();
             this.ValuesListBox = new System.Windows.Forms.ListBox();
             this.ClearButton = new System.Windows.Forms.Button();
+            this.TotalLabel = new System.Windows.Forms.Label();
+            this.AverageLabel = new System.Windows.Forms.Label();
+            this.HighestLabel = new System.Windows.Forms.Label();
+            this.LowestLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // DisplayButton
             // 
             this.DisplayButton.Location = new System.Drawing.Point(72, 286);
             this.DisplayButton.Name = "DisplayButton";
-            this.DisplayButton.Size = new System.Drawing.Size(166, 41);
+            this.DisplayButton.Size = new System.Drawing.Size(170, 41);
             this.DisplayButton.TabIndex = 0;
             this.DisplayButton.Text = "Display Values";
             this.DisplayButton.UseVisualStyleBackColor = true;
@@ -47,19 +51,19 @@
             // 
             // CalculateButton
             // 
-            this.CalculateButton.Location = new System.Drawing.Point(415, 286);
+            this.CalculateButton.Location = new System.Drawing.Point(411, 286);
             this.CalculateButton.Name = "CalculateButton";
-            this.CalculateButton.Size = new System.Drawing.Size(166, 41);
+            this.CalculateButton.Size = new System.Drawing.Size(170, 41);
             this.CalculateButton.TabIndex = 1;
-            this.CalculateButton.Text = "Calculate Values";
+            this.CalculateButton.Text = "Calculate";
             this.CalculateButton.UseVisualStyleBackColor = true;
             this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
             // 
             // ExitButton
             // 
-            this.ExitButton.Location = new System.Drawing.Point(415, 339);
+            this.ExitButton.Location = new System.Drawing.Point(411, 339);
             this.ExitButton.Name = "ExitButton";
-            this.ExitButton.Size = new System.Drawing.Size(166, 41);
+            this.ExitButton.Size = new System.Drawing.Size(170, 41);
             this.ExitButton.TabIndex = 2;
             this.ExitButton.Text = "Exit";
             this.ExitButton.UseVisualStyleBackColor = true;
@@ -69,26 +73,66 @@
             // 
             this.ValuesListBox.FormattingEnabled = true;
             this.ValuesListBox.ItemHeight = 20;
-            this.ValuesListBox.Location = new System.Drawing.Point(243, 15);
+            this.ValuesListBox.Location = new System.Drawing.Point(72, 12);
             this.ValuesListBox.Name = "ValuesListBox";
-            this.ValuesListBox.Size = new System.Drawing.Size(166, 244);
+            this.ValuesListBox.Size = new System.Drawing.Size(170, 244);
             this.ValuesListBox.TabIndex = 3;
             // 
             // ClearButton
             // 
             this.ClearButton.Location = new System.Drawing.Point(72, 339);
             this.ClearButton.Name = "ClearButton";
-            this.ClearButton.Size = new System.Drawing.Size(166, 41);
+            this.ClearButton.Size = new System.Drawing.Size(170, 41);
             this.ClearButton.TabIndex = 4;
             this.ClearButton.Text = "Clear";
             this.ClearButton.UseVisualStyleBackColor = true;
             this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // TotalLabel
+            // 
+            this.TotalLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TotalLabel.Location = new System.Drawing.Point(411, 12);
+            this.TotalLabel.Name = "TotalLabel";
+            this.TotalLabel.Size = new System.Drawing.Size(170, 28);
+            this.TotalLabel.TabIndex = 5;
+            this.TotalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // AverageLabel
+            // 
+            this.AverageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.AverageLabel.Location = new System.Drawing.Point(411, 89);
+            this.AverageLabel.Name = "AverageLabel";
+            this.AverageLabel.Size = new System.Drawing.Size(170, 28);
+            this.AverageLabel.TabIndex = 6;
+            this.AverageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // HighestLabel
+            // 
+            this.HighestLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.HighestLabel.Location = new System.Drawing.Point(411, 156);
+            this.HighestLabel.Name = "HighestLabel";
+            this.HighestLabel.Size = new System.Drawing.Size(170, 28);
+            this.HighestLabel.TabIndex = 7;
+            this.HighestLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // LowestLabel
+            // 
+            this.LowestLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LowestLabel.Location = new System.Drawing.Point(411, 228);
+            this.LowestLabel.Name = "LowestLabel";
+            this.LowestLabel.Size = new System.Drawing.Size(170, 28);
+            this.LowestLabel.TabIndex = 8;
+            this.LowestLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // SalesAnalysisForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(665, 436);
+            this.Controls.Add(this.LowestLabel);
+            this.Controls.Add(this.HighestLabel);
+            this.Controls.Add(this.AverageLabel);
+            this.Controls.Add(this.TotalLabel);
             this.Controls.Add(this.ClearButton);
             this.Controls.Add(this.ValuesListBox);
             this.Controls.Add(this.ExitButton);
@@ -107,6 +151,10 @@
         private System.Windows.Forms.Button ExitButton;
         private System.Windows.Forms.ListBox ValuesListBox;
         private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Label TotalLabel;
+        private System.Windows.Forms.Label AverageLabel;
+        private System.Windows.Forms.Label HighestLabel;
+        private System.Windows.Forms.Label LowestLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -18,9 +18,79 @@ namespace SalesAnalysis
             InitializeComponent();
         }
 
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            ValuesListBox.Items.Clear();
+            TotalLabel.Text = "";
+            AverageLabel.Text = "";
+            HighestLabel.Text = "";
+            LowestLabel.Text = "";
+        }
+
         private void ExitButton_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private double Total(double[] salesArray)
+        {
+            int index;
+            double sum = 0.0;
+
+            for (index = 0; index < salesArray.Length; index++)
+            {
+                sum += salesArray[index];
+            }
+
+            return sum;
+        }
+
+        private double Average(double[] salesArray)
+        {
+            int index;
+            double avrg = 0.0;
+            double average;
+
+            for (index = 0; index < salesArray.Length; index++)
+            {
+                avrg += salesArray[index];
+            }
+
+            average = avrg / salesArray.Length;
+
+            return average;
+        }
+
+        private double Highest(double[] salesArray)
+        {
+            int index;
+            double highest = salesArray[0];
+
+            for (index = 1; index < salesArray.Length; index++)
+            {
+                if (salesArray[index] > highest)
+                {
+                    highest = salesArray[index];
+                }
+            }
+
+            return highest;
+        }
+
+        private double Lowest(double[] salesArray)
+        {
+            int index;
+            double lowest = salesArray[0];
+
+            for (index = 1; index < salesArray.Length; index++)
+            {
+                if (salesArray[index] < lowest)
+                {
+                    lowest = salesArray[index];
+                }
+            }
+
+            return lowest;
         }
 
         private void DisplayButton_Click(object sender, EventArgs e)
@@ -57,15 +127,43 @@ namespace SalesAnalysis
 
         private void CalculateButton_Click(object sender, EventArgs e)
         {
-            double[] numbers = {  };
- 
+            try
+            {
+                const int ARR_SIZE = 7;
+                double[] results = new double[ARR_SIZE];
+                int index = 0;
+                double totalSum, aver, high, low;
+                StreamReader input;
 
+                input = File.OpenText("Sales.txt");
 
-        }
+                while (!input.EndOfStream && index < results.Length)
+                {
+                    results[index] = double.Parse(input.ReadLine());
+                    index++;
+                }
 
-        private void ClearButton_Click(object sender, EventArgs e)
-        {
-            ValuesListBox.Items.Clear();
+                input.Close();
+
+                foreach (double numbers in results)
+                {
+                    ValuesListBox.Items.Add(numbers);
+                }
+
+                totalSum = Total(results);
+                aver = Average(results);
+                high = Highest(results);
+                low = Lowest(results);
+
+                TotalLabel.Text = totalSum.ToString();
+                AverageLabel.Text = aver.ToString("n2");
+                HighestLabel.Text = high.ToString();
+                LowestLabel.Text = low.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
